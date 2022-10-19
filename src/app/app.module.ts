35bfc7342ab88d86fc56101d66bfa4819f967517
Gonzalo <gonzalo.smeriglio@gmail.com>
2022-10-19 17:23:59 -0300

    Se agrega el proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FormsModule } from '@angular/forms';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    AppComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      "backgroundColor": "#F1F1F1",
+      "backgroundPadding": -18,
+      "radius": 60,
+      "toFixed": 2,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#FF6347",
+      "innerStrokeColor": "#32CD32",
+      "innerStrokeWidth": 1,
+      "startFromZero": false})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
